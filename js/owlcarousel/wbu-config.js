1c61605148a3876669fc076da52fd6d2eb5041e1
stephane888 <kksasteph888@gmail.com>
2021-05-07 15:02:07 +0100

    ajout du scroll à ledition des layouts

--- js/owlcarousel/wbu-config.js
+++ js/owlcarousel/wbu-config.js
@@ -3,12 +3,13 @@ jQuery(document).ready(function ($) {
 	$('.owl-carousel').each(function (i, v) {
 		var self = this;
 		var params = $(self).data('owlresponsive');
-		console.log('OWL responsive : ', params);
+		// console.log('OWL responsive : ', params);
 		// example : data-owlresponsive='{"items":1,"nav":false,"dots":true,"loop":true,"autoplay":true}'
+		var textNav='<svg class="owl-svg-nav" viewBox="0 0 12 12"><path fill-rule="evenodd" d="M10.423 8.764a.811.811 0 01-1.145 0l-3.153-3.14-3.153 3.14a.811.811 0 11-1.145-1.15l4.298-4.28 4.298 4.28a.811.811 0 010 1.15z" clip-rule="evenodd"></path></svg>';
 		if (params) {
 			params['navText'] = [
-				'<i class="fa fa-angle-left" aria-hidden="true"></i>',
-				'<i class="fa fa-angle-right" aria-hidden="true"></i>'
+				textNav,
+				textNav
 			];
 			/***
 			 * after the plugin has initialized.
