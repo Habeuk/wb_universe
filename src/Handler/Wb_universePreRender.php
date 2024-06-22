29b56fde8a139511509adf536561298a308ef61c
kouwa stephane <kksasteph888@gmail.com>
2024-06-22 18:00:21 +0200

    MAJ

---
+++ src/Handler/Wb_universePreRender.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\wb_universe\Handler;
+
+use Drupal\Core\Security\TrustedCallbackInterface;
+
+/**
+ *
+ * @author stephane
+ *        
+ */
+class Wb_universePreRender implements TrustedCallbackInterface {
+  
+  /**
+   * Prerender callback for status_messages placeholder.
+   *
+   * @param array $element
+   *        A renderable array.
+   *        
+   * @return array The updated renderable array containing the placeholder.
+   */
+  public static function messagePlaceholder(array $element) {
+    if (isset($element['fallback']['#markup'])) {
+      $element['fallback']['#markup'] = '<div data-drupal-messages-fallback class="hidden messages-list+++"></div>';
+    }
+    // dd($element);
+    return $element;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    return [
+      'messagePlaceholder'
+    ];
+  }
+}
